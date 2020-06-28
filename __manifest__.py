da19f1c393f6d97b0620a935a9d3b2c7248acc8f
Unkonwn-Soldier <52275200+Unkonwn-Soldier@users.noreply.github.com>
2020-06-28 15:16:59 +0800

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Menu Theme",
+
+    'summary': """Odoo Menu Theme""",
+
+    'author': "shaka",
+
+    "category": "Themes/Backend",
+    "installable": True,
+    'version': '1.0',
+    'depends': ["web"],
+    'data': [
+        'views/oejia_menu.xml',
+        'views/template.xml',
+    ],
+    'demo': [
+    ],
+}
